6ce6f90875959aa885aeff8bb3c36d3ebd5bf812
limaraquell <raqueldol@outlook.com>
2026-09-17 23:50:52 -0300

    Create Funcionario.java

---
+++ Exercicio09/Funcionario.java
@@ -0,0 +1,14 @@
+public class Funcionario {
+
+    private String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
