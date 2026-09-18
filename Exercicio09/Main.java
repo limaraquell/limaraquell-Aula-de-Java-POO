98ffb662c533ed062ea20705b062942f1f51ac35
limaraquell <raqueldol@outlook.com>
2026-09-17 23:51:51 -0300

    Create Main.java

---
+++ Exercicio09/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente(
+            "Maria",
+            8000.00,
+            "Tecnologia"
+        );
+
+        gerente.exibirDados();
+    }
+}
