078e4de29743e8ed7fcda00342981b78284efbff
limaraquell <raqueldol@outlook.com>
2026-09-17 23:29:40 -0300

    Create Main.java

---
+++ Exercicio06/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Computador computador = new Computador(
+            "Dell",
+            4500.00,
+            "Intel Core i5",
+            3.5
+        );
+
+        computador.exibirDados();
+    }
+}
