a91284d07afdd112d7f42c1e822c1f22aa43a596
limaraquell <raqueldol@outlook.com>
2026-09-17 23:32:32 -0300

    Create Aluno.java

---
+++ Exercicio07/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno extends Pessoa {
+
+    private String curso;
+
+    public Aluno(String nome, int idade, String curso) {
+        super(nome, idade);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+}
