d1f7e1e58bd118893e1f943ad5c500c5c2356e86
limaraquell <raqueldol@outlook.com>
2026-09-17 23:13:54 -0300

    Create Endereco.java

---
+++ Exercicio05/Endereco.java
@@ -0,0 +1,24 @@
+public class Endereco {
+
+    private String rua;
+    private int numero;
+    private String cidade;
+
+    public Endereco(String rua, int numero, String cidade) {
+        this.rua = rua;
+        this.numero = numero;
+        this.cidade = cidade;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+}
