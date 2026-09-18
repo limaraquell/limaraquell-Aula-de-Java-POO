1792d8385cf99c49e01d806c64ca9a5a4e06b9c6
limaraquell <raqueldol@outlook.com>
2026-09-17 21:58:59 -0300

    Create Produto.java

---
+++ Exercicio01/Produto.java
@@ -0,0 +1,17 @@
+public class Produto {
+    String nome;
+    double preco;
+    int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$ " + preco);
+        System.out.println("Quantidade: " + quantidade);
+    }
+}
